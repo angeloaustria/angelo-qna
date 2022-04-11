62d0b82e0070300d85b92387229e0ba53ed174cd
angeloaustria <angelogabriel.austria@gmail.com>
2022-04-11 19:45:08 -0400

    Message on GET '/' and upload model.nlp

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ manager.load();
 app.use(express.json());
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
+app.get('/', (_, res) => {
+    res.send('Running angelo-qna server...');
+});
+
 app.post('/angeloqna', async (req, res) => {
     const response = await manager.process('en', req.body.message);
     res.send({

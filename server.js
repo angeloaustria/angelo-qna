07bd66d4cc307c5e29aecb54e3681de2458a51bc
angeloaustria <angelogabriel.austria@gmail.com>
2022-04-11 23:17:29 -0400

    Try cors options

--- server.js
+++ server.js
@@ -7,17 +7,21 @@ const port = process.env.PORT || 5000;
 const manager = new NlpManager({ languages: ['en'] });
 manager.load();
 
-app.use(cors());
 app.use(express.json());
+const corsOptions = {
+    origin: 'https://angelo-qna.herokuapp.com',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 app.get('/', (_, res) => {
     res.send('Running angelo-qna server...');
 });
 
-app.post('/angeloqna', async (req, res) => {
+app.post('/angeloqna', cors(corsOptions), async (req, res) => {
     const response = await manager.process('en', req.body.message);
-    res.json({
+    res.send({
         answer: response.answers[0].answer
     });
 });

72536e9684f31767a233370025ec62197d47d28b
angeloaustria <angelogabriel.austria@gmail.com>
2022-04-12 00:14:21 -0400

    Playing around with cors

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 const manager = new NlpManager({ languages: ['en'] });
 manager.load();
 
-app.use(cors());
+app.use(cors({origin: true, credentials: true}));
 app.use(express.json());
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
